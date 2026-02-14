dc3c206d940815a21cb01439410e34cc4eead59a
Zishan <97729148+zishanAl@users.noreply.github.com>
2026-02-14 10:04:29 +0530

    Implement submatrix sum calculation
    Added input for coordinates and calculated submatrix sum.

--- prefixSum.cpp
+++ prefixSum.cpp
@@ -11,18 +11,26 @@ for (int i = 1; i <= n; ++i) {
     }
 }
 
+int x1,y1,x2,y2;
+cin>>x1>>y1>>x2>>y2;
+
+int ans = prefixSum[x2][y2] - prefixSum[x1-1][y2] 
+          - prefixSum[x2][y1-1] + prefixSum[x1-1][y1-1];
+                  
+cout<<ans<<endl;
+
 
 // Submatrix Sum
-for (int r1 = 1; r1 <= R; ++r1) {
-    for (int r2 = r1; r2 <= R; ++r2) {
-        for (int c1 = 1; c1 <= C; ++c1) {
-            for (int c2 = c1; c2 <= C; ++c2) {
-                long long sum = prefixSum[r2][c2]
-                              - prefixSum[r1 - 1][c2]
-                              - prefixSum[r2][c1 - 1]
-                              + prefixSum[r1 - 1][c1 - 1];
-                // condition as per question
-            }
-        }
-    }
-}
+// for (int r1 = 1; r1 <= R; ++r1) {
+//     for (int r2 = r1; r2 <= R; ++r2) {
+//         for (int c1 = 1; c1 <= C; ++c1) {
+//             for (int c2 = c1; c2 <= C; ++c2) {
+//                 long long sum = prefixSum[r2][c2]
+//                               - prefixSum[r1 - 1][c2]
+//                               - prefixSum[r2][c1 - 1]
+//                               + prefixSum[r1 - 1][c1 - 1];
+//                 // condition as per question
+//             }
+//         }
+//     }
+// }
